887c6ad1e0cfd8bb43e878b214cd50a71e85ea82
aleksandar mitev <aleksandar.mitev.mk@gmail.com>
2020-04-14 21:00:53 +0200

    Updates

---
+++ precache-manifest.bc51ca1df9f52f8d15094913ee12e782.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3b8f93bba601c6c40002296ad0cf518",
+    "url": "/Team_building-game/index.html"
+  },
+  {
+    "revision": "38d798b89d03541179a0",
+    "url": "/Team_building-game/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "24fb3b25b1fb2217047e",
+    "url": "/Team_building-game/static/css/main.13d87222.chunk.css"
+  },
+  {
+    "revision": "38d798b89d03541179a0",
+    "url": "/Team_building-game/static/js/2.5903e578.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/Team_building-game/static/js/2.5903e578.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24fb3b25b1fb2217047e",
+    "url": "/Team_building-game/static/js/main.bd394b64.chunk.js"
+  },
+  {
+    "revision": "c7b6c8d253ee719725f0",
+    "url": "/Team_building-game/static/js/runtime-main.5782bb82.js"
+  },
+  {
+    "revision": "0a4b034f1d9474d72e8bf79b6c685705",
+    "url": "/Team_building-game/static/media/0.0a4b034f.jpg"
+  },
+  {
+    "revision": "b0444bbdb68176421ea3fd2623ed6730",
+    "url": "/Team_building-game/static/media/1.b0444bbd.jpg"
+  },
+  {
+    "revision": "8b1bb81cc7365e4ec1bd778e4382224a",
+    "url": "/Team_building-game/static/media/10.8b1bb81c.jpg"
+  },
+  {
+    "revision": "428b72aaa3c49d2dc6bc2a8787b26ddd",
+    "url": "/Team_building-game/static/media/11.428b72aa.jpg"
+  },
+  {
+    "revision": "bdecbe5f8cb5fe0796fbf29ab2774794",
+    "url": "/Team_building-game/static/media/12.bdecbe5f.jpg"
+  },
+  {
+    "revision": "d7f633e2638ee0948a045ae1607483e3",
+    "url": "/Team_building-game/static/media/13.d7f633e2.jpg"
+  },
+  {
+    "revision": "8b6500f53556c9e8ad632fe5a62a0c5a",
+    "url": "/Team_building-game/static/media/14.8b6500f5.jpg"
+  },
+  {
+    "revision": "89e06c7f2e705c3f135207ef8f40cc9f",
+    "url": "/Team_building-game/static/media/15.89e06c7f.jpg"
+  },
+  {
+    "revision": "b9da4be8f2db5dc261c2df940de7073c",
+    "url": "/Team_building-game/static/media/16.b9da4be8.jpg"
+  },
+  {
+    "revision": "faa68e52f2eeded45f45aa1517d1e5bc",
+    "url": "/Team_building-game/static/media/17.faa68e52.jpg"
+  },
+  {
+    "revision": "7b4d2839f43bd1135f28717d741dc52f",
+    "url": "/Team_building-game/static/media/18.7b4d2839.jpg"
+  },
+  {
+    "revision": "5ecdf084e443a3436d33b1582c30c1e8",
+    "url": "/Team_building-game/static/media/2.5ecdf084.jpg"
+  },
+  {
+    "revision": "612aea5cecc2dbdd52d1ac165414d83e",
+    "url": "/Team_building-game/static/media/3.612aea5c.jpg"
+  },
+  {
+    "revision": "8bfc9254d45cb54090f2b38242c07bc1",
+    "url": "/Team_building-game/static/media/4.8bfc9254.jpg"
+  },
+  {
+    "revision": "ca12ce2bed1f9c7f236b17baac19a324",
+    "url": "/Team_building-game/static/media/5.ca12ce2b.jpg"
+  },
+  {
+    "revision": "60ea5ad477a6d1c408a106888f8ef3a1",
+    "url": "/Team_building-game/static/media/6.60ea5ad4.jpg"
+  },
+  {
+    "revision": "d476dac69ea4bc64c0ff4d5839c19b32",
+    "url": "/Team_building-game/static/media/7.d476dac6.jpg"
+  },
+  {
+    "revision": "c98e1e51c3984fccd14d9acdb2aa8f0b",
+    "url": "/Team_building-game/static/media/8.c98e1e51.jpg"
+  },
+  {
+    "revision": "6e9837c295596ca48d01c8a9fc90914e",
+    "url": "/Team_building-game/static/media/9.6e9837c2.jpg"
+  },
+  {
+    "revision": "dcd3727437d20b4817cd5c9c7787717b",
+    "url": "/Team_building-game/static/media/logo.dcd37274.png"
+  },
+  {
+    "revision": "ecf3cd424eeeeba5ee737b0dbea75a1d",
+    "url": "/Team_building-game/static/media/map.ecf3cd42.jpg"
+  }
+]);
